2cb4e0c624504fb346dc585005085ea5c5c1b2d3
super-magistro <135324311+super-magistro@users.noreply.github.com>
2026-02-05 21:23:33 +0100

    [Feat] add login page layout with responsive design and styling

---
+++ front/app/pages/login.vue
@@ -0,0 +1,20 @@
+<style lang="less" scoped src="~/assets/less/components/pages/login.less"></style>
+<template>
+  <div class="page-container">
+
+    <div class="left-panel">
+      <div class="overlay">
+        <blockquote>"La stratégie est le chemin vers le Valhalla."</blockquote>
+      </div>
+      <img
+          src="https://images.unsplash.com/photo-1646221496474-f1980f9b5be5?auto=format&fit=crop&q=80&w=1080"
+          alt="Jeu de stratégie Viking"
+      />
+    </div>
+
+    <div class="right-panel">
+      <MoleculesLoginForm />
+    </div>
+
+  </div>
+</template>
